6948253f42e9bf453ee8680ee581eff2a30d8d5c
unknown <roshan.bazgain@gmail.com>
2022-12-20 20:22:44 +0545

    Modofy the logic

--- Sum.c
+++ Sum.c
@@ -4,12 +4,14 @@ int main()
 
     int number1, number2, sum;
 
-    printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
+    printf("Enter First number: ");
+    scanf("%d", &number1);
+    printf("Enter second number: ");
+    scanf("%d", &number2);
 
     // calculating sum
     sum = number1 + number2;
 
-    printf("%d + %d = %d", number1, number2, sum);
+    printf("%d + %d = %d\t", number1, number2, sum);
     return 0;
 }
